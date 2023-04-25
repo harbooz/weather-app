24769651ec56eebbad3e9f6f26463add4f51c6b8
Alin Stoica <Alin.Stoica@888holdings.com>
2023-04-25 12:39:34 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [location, setLocation] = useState("");
+
+  const [img, setImg] = useState(false);
+
+  const url = `http://api.weatherstack.com/current?access_key=914ab9023f692bd893de4e2d0bf12459&query=${location}`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="weather-app">
+      <h2 className="title-app">Weather app</h2>
+      <div className="searchLocation">
+        <input
+          type="text"
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyUp={searchLocation}
+          placeholder="Find city ..."
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <div className="city">{data.location?.name}</div>
+          </div>
+          <div className="temp">
+            <h4>
+              <img
+                src={data.current?.weather_icons}
+                alt={data.current?.weather_descriptions}
+              />
+            </h4>
+
+            <h1> {data.current?.temperature}°C</h1>
+          </div>
+          <div className="description">
+            {data.current?.weather_descriptions}
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            Feels like: <p>{data.current?.feelslike}°C</p>
+          </div>
+          <div className="humidity">
+            Humidity <p>{data.current?.humidity}%</p>
+          </div>
+          <div className="wind">
+            Wind speed
+            <p>{data.current?.wind_speed} km/h</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
